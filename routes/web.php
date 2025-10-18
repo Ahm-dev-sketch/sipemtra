20cb341cda1ab34051d9315cb2ef34169b793d20
unknown <novrianto.sapta0501@student.unri.ac.id>
2025-10-18 15:42:11 +0700

    Membuat halaman login

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () {
+//    return view('welcome');
+//})
+// Auth
+Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
+Route::post('/login', [AuthController::class, 'login']);
+Route::get('/register', [AuthController::class, 'showRegister'])->name('register');
+Route::post('/register', [AuthController::class, 'register']);
+
+Route::get('password/reset', [AuthController::class, 'showForgotPasswordForm'])->name('password.request');
+
