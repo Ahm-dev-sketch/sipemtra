0882b74ec7180d47799fcd02a521f631a2f8c529
Qoulan <qoulan.tsaqila3286@student.unri.ac.id>
2025-10-20 14:09:17 +0700

    nambah route kelola pelanggan

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,10 @@ Route::post('/register/verify', [AuthController::class, 'verifyRegisterOtp'])->n
 
 Route::get('/bookings', [AdminController::class, 'bookings'])->name('bookings');
 Route::put('/bookings/{booking}', [AdminController::class, 'updateBooking'])->name('bookings.update');
+
+Route::get('/pelanggan', [AdminController::class, 'pelanggan'])->name('pelanggan');
+Route::get('/pelanggan/create', [AdminController::class, 'createPelanggan'])->name('pelanggan.create');
+Route::post('/pelanggan', [AdminController::class, 'storePelanggan'])->name('pelanggan.store');
+Route::get('/pelanggan/{customer}/edit', [AdminController::class, 'editPelanggan'])->name('pelanggan.edit');
+Route::put('/pelanggan/{customer}', [AdminController::class, 'updatePelanggan'])->name('pelanggan.update');
+Route::delete('/pelanggan/{customer}', [AdminController::class, 'destroyPelanggan'])->name('pelanggan.destroy');
